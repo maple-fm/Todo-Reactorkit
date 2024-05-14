8d3cb2986fd0410b7b9b5af19a563980615d9951
maple-fm <fuma.deguchi@gmail.com>
2024-05-14 19:50:13 +0900

    Initial Commit

---
+++ Todo-Reactorkit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todo-Reactorkit
+//
+//  Created by 出口楓真 on 2024/05/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todo-Reactorkit/Todo_ReactorkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ReactorkitApp.swift
+//  Todo-Reactorkit
+//
+//  Created by 出口楓真 on 2024/05/14.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ReactorkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
